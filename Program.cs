48bee38500959ef691f266323ee4a35f5e752e19
leeool <lgonsalez02@gmail.com>
2022-04-01 21:54:56 -0300

    finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿int numero;
+
+Console.Write("Insira um número: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+bool numeropar = (numero % 2) == 0;
+
+if (numeropar)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"{numero} é um número par");
+
+}
+
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"{numero} é um número ímpar");
+}
+
+Console.ResetColor();
